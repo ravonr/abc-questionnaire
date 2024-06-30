f865f4bf5a3e735d00bb30baad24c6ca1e512bc7
ravon <135451054+ravonr@users.noreply.github.com>
2024-06-30 08:36:53 -0400

    Update Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -15,11 +15,15 @@ const Footer = () => {
       </p>
       <br />
       <p>
-        site inspired by <a
-          href="https://a-website-is-a-room.net/"
+        site inspired by 
+        <a href="https://a-website-is-a-room.net/"
           target="_blank"
           rel="noopener noreferrer"
-          className="underline underline-offset-2	">a website is a room</a>
+          className="underline underline-offset-2">a website is a room</a> and 
+        <a href="https://chimeric-worlding.netlify.app/" 
+          target="blank" 
+          rel="noopener noreferrer"
+          className="underline underline-offset-2">chimeric worlding</a>
         . web design by{' '}
         <a
           href="https://studiotibby.com/"
